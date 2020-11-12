a819a80585419b01988bd3317fdc6a2457098f86
Erik <eroempagel@outlook.com>
2020-11-12 11:07:23 -0600

    demo minus assessment rundown

---
+++ assets/javascript/main.js
@@ -0,0 +1,59 @@
+
+/*
+let user = {
+    firstName: "Test",
+    lastName: "User",
+    userName: "mr_bob",
+    age: 45,
+    email: "bob@gmail.com",
+    sayName: function() {
+        //console.log(this.firstName + ' ' + this.lastName)
+        console.log(`${this.firstName} ${this.lastName}`)
+    }
+}
+
+console.log(user.email);
+let findProperty = "email";
+
+console.log(user[findProperty]);
+
+user.firstName = 'Bob'
+
+console.log(user)
+
+user.sayName();
+*/
+
+let  papaCat = {
+    colorStyle: "Calico",
+    colors: ["white", "grey", "black"],
+    legs: 4,
+    eyeColor: 'none',
+    meowsAlot: true,
+    breed: "Siamese",
+    name: 'Tom',
+    destruction: 100,
+    listenToProblems: true,
+    getAdopted: function() {
+        if (this.listenToProblems === true && this.destruction < 100){
+            return true;
+        } else {
+            return false;
+        }
+    }, 
+    declaw: function() {
+        this.destruction -= 50; // this.destuction = this.destruction - 50
+    },
+    crossTheRoad: function() {
+        if (Math.random() < .25 && this.legs > 0) {
+            this.legs--;
+        }
+    }
+}
+
+papaCat.crossTheRoad();
+
+papaCat.declaw();
+console.log(papaCat.getAdopted());
+
+console.log(papaCat)
